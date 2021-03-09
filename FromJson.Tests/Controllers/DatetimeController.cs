93b27783e69a828ca099ca44d890ee9c0aba86b4
clk <617682513@qq.com>
2021-03-09 12:58:25 +0800

    add test

---
+++ FromJson.Tests/Controllers/DatetimeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FromJson.Tests.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class DatetimeController: ControllerBase
+    {
+
+    }
+}
---
+++ FromJson.Tests/Controllers/StringController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FromJson.Tests.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class StringController:ControllerBase
+    {
+        [HttpPost]
+        public string PostStringInArgument([FromJson]string text)
+        {
+            return text;
+        }
+
+        [HttpPut]
+        public string PutStringInArgument([FromJson] string text)
+        {
+            return text;
+        }
+
+
+    }
+}
